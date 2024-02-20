bb9074c97d1f8ca32d4304430c7f42aad2c8eb68
skulibas <107680213+skulibas@users.noreply.github.com>
2024-02-19 22:33:27 -1000

    Update Lojban.java

--- Lojban.java
+++ Lojban.java
@@ -1,28 +1,28 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * OFFICE HOURS QUESTIONS:
  * 1) When you say implement the following pre-defined short words "lo" and "se," do you mean that our program has to put these words in? The user does not need to put these words in? For example, the user would input .name. and our code would automatically know that there is a "lo" before it.
+ * 2) “i se 2 sumji lo .answer. 2.” assigns 4 to “answer”. -- doesnt this statement mean assigns the variable answer to 4? since se would switch 2 and lo .answer.
  */
 
 public class Lojban {
-    HashMap<String, String> predicates = new HashMap<String, String>();
+    // Predicates/variables are represented as a string
+    // First List is the list of arguments associated with that predicate
+    // Second nested list is the actual arguments themselves
+    private Map<String, List<List<Object>>> database = new HashMap<>();
 
-    public static String[] evaluate(String str) {
+    public static List<String> evaluate(String input) {
         // lowercase the string, treating upper and lower cases the same
-        String lowerCaseStr = str.toLowerCase();
+        String lowerCaseInput = input.toLowerCase();
 
         //Check if each character in the string is valid
-        for (int i = 0; i < str.length(); i++) {
-            validInput(lowerCaseStr.charAt(i));
+        for (int i = 0; i < input.length(); i++) {
+            validInput(lowerCaseInput.charAt(i));
         }
 
-        String[] arrStr = str.split(" ");
-        for(String string : arrStr) {
-
-        }
-        return arrStr;
+        String[] arrStr = lowerCaseInput.trim().split("(?<=i\\s+)");
+        return Arrays.asList(arrStr);
     }
 
     /**
@@ -48,9 +48,9 @@ public class Lojban {
         System.out.print("Enter an input: ");
         Scanner input = new Scanner(System.in);
         System.out.println("");
+        String str = input.nextLine();
 
-        String str = input.next();
-        String[] arr = evaluate(str);
-        input.close();
+        // Create a new instance of Lojban
+        Lojban logicProcessor = new Lojban();
     }
 }
